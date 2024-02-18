f4f2e34eff927a227c9f35969eda130c01f4f995
Yana Pavlova <yn-pvlv@yandex.ru>
2024-02-18 23:03:34 +0300

    Добавить карточку вручную Удалить карточку по клику на кнопку удаления

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 // @todo: Темплейт карточки
 
+// клонируем содержимое тега template
+
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
@@ -7,3 +9,31 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content; // забрали шаблон
+const cards = document.querySelector('.places__list'); // забрали контейнер, в который будем класть карточки
+
+// функция добавления карточки
+function addCard(removeCard) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true); // склонировали содержимое темплейта карточки
+
+  cardElement.querySelector('.card__image').src = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'; // положили урл картинки
+  cardElement.querySelector('.card__title').textContent = 'Архыз'; // положили тайтл
+  
+  cards.append(cardElement); // показали карточку на странице
+
+  const card = cards.querySelector('.card'); // нашли карточку
+  const deleteButton = card.querySelector('.card__delete-button'); // нашли кнопку удаления
+
+  deleteButton.addEventListener('click', removeCard); // добавили обработчик события для кнопки удаления
+
+};
+
+function removeCard() {
+  const card = cards.querySelector('.card'); // нашли карточку; код повторяется - плохо
+  const deleteButton = card.querySelector('.card__delete-button'); // нашли кнопку удаления; код повторяется - плохо
+  const cardItem = deleteButton.closest('.card');
+  cardItem.remove();
+}; // добавили функцию удаления ближайшей карточки-родителя по отношению к нажатой кнопке
+
+addCard(removeCard); // вызвали функцию добавления карточки и передали ей функцию удаления карточки в качестве аргумента
