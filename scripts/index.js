ab5d3a7bb9da5226342447b1b3878c941c14a0a1
Yana Pavlova <yn-pvlv@yandex.ru>
2024-02-20 18:43:53 +0300

    Разделена задача создания и добавления карточки Одна функция создаёт карточку Другая функция добавляет её на страницу

--- scripts/index.js
+++ scripts/index.js
@@ -13,22 +13,24 @@
 const cardTemplate = document.querySelector('#card-template').content; // забрали шаблон
 const cards = document.querySelector('.places__list'); // забрали контейнер, в который будем класть карточки
 
-// функция добавления карточки
-function addCard(cardsData, removeCard) {
+// функция создания карточки
+function createCard(cardsData, removeCard) {
   // cardsData это данные ОДНОЙ карточки
-
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true); // склонировали содержимое темплейта карточки
 
   cardElement.querySelector('.card__image').src = cardsData['link']; // положили урл картинки
   cardElement.querySelector('.card__image').alt = cardsData['alt']; // положили alt в картинку
   cardElement.querySelector('.card__title').textContent = cardsData['name']; // положили тайтл
-  
-  cards.append(cardElement); // показали карточку на странице
-
   const deleteButton = cardElement.querySelector('.card__delete-button'); // нашли кнопку удаления
 
   deleteButton.addEventListener('click', removeCard); // добавили обработчик события для кнопки удаления
 
+  return cardElement;
+}
+
+// функция добавления карточки на странице
+function addCard(cardElement) {  
+  cards.append(cardElement); // положили карточку как последний элемент в контейнер карточек
 };
 
 // добавили функцию удаления ближайшей карточки-родителя по отношению к нажатой кнопке; нажатую кнопку получили через дефолтный аргумент event, который есть у каждого addEventListener
@@ -36,7 +38,7 @@ function removeCard(event) {
   event.target.closest('.card').remove();
 };
 
-// foreach перебрал массив и для каждой карточки вызвал функцию  добавления карточки и передал ей данные карточки и функцию удаления карточки в качестве аргументов
+// foreach перебрал массив и для каждой карточки вызвал функцию  добавления карточки и передал ей функцию создания карточки в качестве аргумента
 initialCards.forEach(card => {
-  addCard(card, removeCard);
+  addCard(createCard(card, removeCard));
 });
